4e5016a92dd562c18cb75b06d6f19247cac5e687
Vishwanathan27 <vishwanathan.venkataraman27@gmail.com>
2023-10-13 01:31:15 +0530

    users

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -22,82 +22,73 @@ const registerUser = async (req, res) => {
 };
 
 const getAllUsers = async (req, res) => {
-  try {
-    const { query } = req;
-    const page = parseInt(query.page) || dataConfig.page;
-    const limit = parseInt(query.limit) || dataConfig.limit;
-    const sort = query.sort || dataConfig.sort;
-    const search = query.search || "";
-    const users = await userService.getUsers(page, limit, search, sort);
-    res.status(200).send({ success: true, users });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({
+  const { query } = req;
+  const page = parseInt(query.page) || dataConfig.page;
+  const limit = parseInt(query.limit) || dataConfig.limit;
+  const sort = query.sort || dataConfig.sort;
+  const search = query.search || "";
+  const { users, error } = await userService.getUsers(
+    page,
+    limit,
+    search,
+    sort
+  );
+  if (error) {
+    return res.status(500).send({
       success: false,
       error: "Internal Server Error",
     });
   }
+  res.status(200).send({ success: true, users });
 };
 
 const getUserById = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const user = await userService.getUserById(id);
-    delete user.password;
-    if (!user) {
-      return res
-        .status(404)
-        .send({ success: false, message: "User not found" });
-    }
-    res.status(200).send({ success: true, user });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({
+  const { id } = req.params;
+  const { user, error } = await userService.getUserById(id);
+  if (error) {
+    return res.status(500).send({
       success: false,
       error: "Internal Server Error",
     });
   }
+  if (!user) {
+    return res.status(404).send({ success: false, message: "User not found" });
+  }
+  delete user.password;
+  res.status(200).send({ success: true, user });
 };
 
 const updateUser = async (req, res) => {
-  try {
-    const { id } = req.params;
-    if (req.body.password) {
-      const hashedPassword = await userService.hashPassword(req.body.password);
-      req.body.password = hashedPassword;
-    }
-    const updatedData = req.body;
-    const user = await userService.updateUser(id, updatedData);
-    delete user.password;
-    if (!user) {
-      return res
-        .status(404)
-        .send({ success: false, message: "User not found" });
-    }
-    res.status(200).send({ success: true, user });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({
+  const { id } = req.params;
+  if (req.body.password) {
+    const hashedPassword = await userService.hashPassword(req.body.password);
+    req.body.password = hashedPassword;
+  }
+  const updatedData = req.body;
+  const { message, error } = await userService.updateUser(id, updatedData);
+
+  if (error) {
+    return res.status(500).send({
       success: false,
       error: "Internal Server Error",
     });
   }
+
+  res.status(200).send({ success: true, message });
 };
 
 const deleteUser = async (req, res) => {
-  try {
-    const { id } = req.params;
-    await userService.deleteUser(id);
-    res
-      .status(200)
-      .send({ success: true, message: "User deleted successfully" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({
+  const { id } = req.params;
+  const { message, error } = await userService.deleteUser(id);
+
+  if (error) {
+    return res.status(500).send({
       success: false,
       error: "Internal Server Error",
     });
   }
+
+  res.status(200).send({ success: true, message });
 };
 
 module.exports = {
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -2,19 +2,32 @@ const { User } = require("@models");
 const bcrypt = require("bcryptjs");
 
 module.exports = {
-  health: async (_, res) => {
-    res.status(201).send("OK");
-  },
-
   register: async (userData) => {
     const user = new User(userData);
     return user.save();
   },
 
-  updateUser: async (userId, updatedData) =>
-    User.findByIdAndUpdate(userId, updatedData, { new: true }),
+  updateUser: async (userId, updatedData) => {
+    try {
+      const user = await User.findByIdAndUpdate(userId, updatedData, {
+        new: true,
+      });
+      return { message: "User successfully updated" };
+    } catch (error) {
+      console.log(error);
+      return { error };
+    }
+  },
 
-  deleteUser: async (userId) => User.findByIdAndRemove(userId),
+  deleteUser: async (userId) => {
+    try {
+      await User.findByIdAndRemove(userId);
+      return { message: "User successfully deleted" };
+    } catch (error) {
+      console.log(error);
+      return { error };
+    }
+  },
 
   hashPassword: async (password) => {
     const salt = await bcrypt.genSalt(10);
@@ -23,13 +36,21 @@ module.exports = {
 
   compareHashPassword: async (password, hash) => bcrypt.compare(password, hash),
 
-  getUserById: async (userId) => User.findById(userId),
+  getUserById: async (userId) => {
+    try {
+      const user = await User.findById(userId);
+      return { user };
+    } catch (error) {
+      console.log(error);
+      return { error };
+    }
+  },
 
   async getUsers(
     page = 1,
     itemsPerPage = 10,
     searchTerm = "",
-    sort = { _id: -1 },
+    sort = { _id: -1 }
   ) {
     try {
       const skip = (page - 1) * itemsPerPage;
@@ -42,7 +63,8 @@ module.exports = {
           },
         };
       }
-      return await User.find(query).skip(skip).limit(limit).sort(sort);
+      const users = await User.find(query).skip(skip).limit(limit).sort(sort);
+      return { users };
     } catch (error) {
       console.log(error);
       return { error };
